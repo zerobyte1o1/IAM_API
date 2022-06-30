55e64285a00c78b2e6af707969bd637bf89e2147
刘方靖 <fj.liu@teletraan.io>
2022-06-30 05:38:59 +0000

    更新’run.sh‘
    适配jenkins

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 pytest testcase/ --alluredir=./result --clean-alluredir
 
-/opt/homebrew/bin/allure generate ./result -c -o ./result/report/
+allure generate ./result -c -o ./result/report/
 
-/opt/homebrew/bin/allure serve ./result/ -p 8080
+allure serve ./result/ -p 8080
