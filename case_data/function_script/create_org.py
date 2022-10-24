12fe4ae83657496e2d3f23b3969778430704af5d
liufangjing <fj.liu@teletraan.io>
2022-10-24 13:49:50 +0800

    修改代码调试方法

--- case_data/function_script/create_org.py
+++ case_data/function_script/create_org.py
@@ -3,29 +3,30 @@ from case_data.management_center_data.organization_data import OrganizationData
 
 # 创建组织
 # 根组织数
-num = 60
+num = 1000
 # 组织深度
 deep = 3
-tenant_code = "1234"
-account = "eam1234"
-password = "teletraan"
+# tenant_code = "comba"
+# account = "comba_admin"
+# password = "comba_admin"
 
 
 # 创建组织
 def create_org(**kwargs):
     org = Organization(**kwargs)
     org_id = org.get_organization_tree_nodes()[0]["id"]
-    for i in range(num):
-        a = OrganizationData(**kwargs)
-        print("正在创建第" + str(i + 1) + "个组织")
-        data = a.create_organization_ask(org_id)
-        new_org_id = org.create_organization_api(data)
-        for j in range(deep):
-            data = a.create_organization_ask(new_org_id)
+    if "-" in org_id:
+        for i in range(num):
+            a = OrganizationData(**kwargs)
+            print("正在创建第" + str(i + 1) + "个组织")
+            data = a.create_organization_ask(org_id)
             new_org_id = org.create_organization_api(data)
-        print(new_org_id)
+            for j in range(deep):
+                data = a.create_organization_ask(new_org_id)
+                new_org_id = org.create_organization_api(data)
+            print(new_org_id)
 
 
 if __name__ == '__main__':
-    create_org(account=account, tenant_code=tenant_code, password=password)
+    create_org()
     print(str(num) + "个组织创建完成。")
